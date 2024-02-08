75f3e7431a395cf221c06252f043a75fd4c5e6d4
RafaelPires2 <rafael.pires@taqtile.com.br>
2024-02-08 08:31:30 -0300

    feat: safe area view

--- App.tsx
+++ App.tsx
@@ -1,7 +1,7 @@
 import React from 'react';
 import { StatusBar } from 'expo-status-bar';
 import { Routes } from './src/app/core/navigation/routes';
-import { SafeAreaProvider } from 'react-native-safe-area-context';
+import { SafeAreaProvider, SafeAreaView } from 'react-native-safe-area-context';
 import { ThemeProvider } from 'styled-components';
 import { commonTheme } from '@atomic/obj.theme';
 
@@ -9,6 +9,7 @@ export default function App() {
   return (
     <ThemeProvider theme={commonTheme}>
       <SafeAreaProvider>
+        <SafeAreaView />
         <StatusBar style="auto" />
         <Routes />
       </SafeAreaProvider>
