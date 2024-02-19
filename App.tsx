7426b61c87740ac06ee2b9fffcf5287953c05a77
RafaelPires2 <rafael.pires@taqtile.com.br>
2024-02-19 07:28:28 -0300

    feat: added fonts and lib expo fonts

--- App.tsx
+++ App.tsx
@@ -1,11 +1,27 @@
 import React from 'react';
+import { useFonts } from 'expo-font';
 import { StatusBar } from 'expo-status-bar';
+import { commonTheme } from '@atomic/obj.theme';
+import { ThemeProvider } from 'styled-components';
 import { Routes } from './src/app/core/navigation/routes';
 import { SafeAreaProvider, SafeAreaView } from 'react-native-safe-area-context';
-import { ThemeProvider } from 'styled-components';
-import { commonTheme } from '@atomic/obj.theme';
 
 export default function App() {
+  const [fontsLoaded] = useFonts({
+    'IBMPlexSans-Regular': require('@assets/fonts/IBMPlexSans-Regular.ttf'),
+    'IBMPlexSans-Medium': require('@assets/fonts/IBMPlexSans-Medium.ttf'),
+    'IBMPlexSans-SemiBold': require('@assets/fonts/IBMPlexSans-SemiBold.ttf'),
+    'IBMPlexSans-Bold': require('@assets/fonts/IBMPlexSans-Bold.ttf'),
+    'IBMPlexSans-Italic': require('@assets/fonts/IBMPlexSans-Italic.ttf'),
+    'IBMPlexSansCondensed-Medium': require('@assets/fonts/IBMPlexSansCondensed-Medium.ttf'),
+    'IBMPlexSansCondensed-Bold': require('@assets/fonts/IBMPlexSansCondensed-Bold.ttf'),
+    'IBMPlexSansCondensed-SemiBold': require('@assets/fonts/IBMPlexSansCondensed-SemiBold.ttf'),
+  });
+
+  if (!fontsLoaded) {
+    return;
+  }
+
   return (
     <ThemeProvider theme={commonTheme}>
       <SafeAreaProvider>
