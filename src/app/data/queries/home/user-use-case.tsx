67532af34bc61e13a38ff5ad80ae6ab0bf273f7e
RafaelPires2 <rafael.pires@taqtile.com.br>
2024-02-26 14:33:46 -0300

    feat: user use case

---
+++ src/app/data/queries/home/user-use-case.tsx
@@ -0,0 +1,34 @@
+import { useEffect, useState } from 'react';
+import axios, { AxiosResponse } from 'axios';
+
+interface UserType {
+  id: string;
+  name: string;
+  email: string;
+}
+
+interface UserProps {
+  idUser: string;
+}
+
+export const useUserQuery = ({ idUser }: UserProps) => {
+  const [user, setUser] = useState<UserType>();
+
+  useEffect(() => {
+    const fetchData = async () => {
+      try {
+        const response: AxiosResponse<UserType[]> = await axios.get(`http://localhost:3000/users?id=${idUser}`);
+        const data = response.data;
+        const currentUser = data.find(u => u.id === idUser);
+
+        setUser(currentUser);
+      } catch (error) {
+        console.error(error);
+      }
+    };
+
+    fetchData();
+  }, []);
+
+  return { user };
+};
